b8db9bc276675fef676caa0d456dba58f7e5fea8
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2022-10-20 13:39:07 +0200

    Bug-fix, removed PrimeFaces dependencies

--- src/main/java/fit/gja/songtrainer/config/SongtrainerAppConfig.java
+++ src/main/java/fit/gja/songtrainer/config/SongtrainerAppConfig.java
@@ -96,7 +96,6 @@ public class SongtrainerAppConfig {
 
         props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
         props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-        props.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
 
         return props;
     }
